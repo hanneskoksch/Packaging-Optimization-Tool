421f3c62e0da3714e32b7239bfa06e038c4978b0
Hannes Koksch <hannes.koksch@gmail.com>
2024-11-11 01:14:32 +0100

    basic tabs structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <></>;
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+import Upload from "./components/Upload";
+
+export function App() {
+  return (
+    <div className="m-4">
+      <Tabs defaultValue="upload" className="w-[600px]">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="upload">1. Upload CSV</TabsTrigger>
+          <TabsTrigger value="check">2. Check data</TabsTrigger>
+          <TabsTrigger value="matrix">3. Matrix</TabsTrigger>
+        </TabsList>
+        <TabsContent value="upload">
+          <Upload />
+        </TabsContent>
+        <TabsContent value="check">Table Component</TabsContent>
+        <TabsContent value="matrix">Matrix Component</TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default App;
