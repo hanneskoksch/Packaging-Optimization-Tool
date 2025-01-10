31fe4715a37f48d607faf0958265d7bea28f5000
Hannes Koksch <hannes.koksch@gmail.com>
2025-01-10 11:48:05 +0100

    improved testing

--- src/experimental/second-approach/calculations.test.ts
+++ src/experimental/second-approach/calculations.test.ts
@@ -1,4 +1,4 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
 import { multiplyVectorMatrix } from "./calculations";
 
 test("NEW APPROACH", () => {
@@ -9,10 +9,10 @@ test("NEW APPROACH", () => {
     [0.3, -0.1, 1],
   ];
 
-  try {
-    const result = multiplyVectorMatrix(vector, matrix);
-    console.log("Result:", result); // Expected output: [0.1, 0.02, 0.03]
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  const result = multiplyVectorMatrix(vector, matrix);
+  expect(result).toEqual([0.1, 0.02, 0.03]);
+  const result2 = multiplyVectorMatrix(result, matrix);
+  expect(result2).toEqual([0.111, 0.037, 0.064]);
+  const result3 = multiplyVectorMatrix(result2, matrix);
+  expect(result3).toEqual([0.1339, 0.0528, 0.1047]);
 });
