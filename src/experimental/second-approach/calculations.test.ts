47b65b4faff6e483f79b07af11439dae2537afec
Hannes Koksch <hannes.koksch@gmail.com>
2025-01-27 22:19:48 +0100

    adjusted tests to check equal with BigNumber

--- src/experimental/second-approach/calculations.test.ts
+++ src/experimental/second-approach/calculations.test.ts
@@ -3,40 +3,65 @@ import { multiplyVectorMatrix } from "./calculations";
 import { bignumber, BigNumber, transpose } from "mathjs";
 
 test("Easy matrix * vector multiplication", () => {
-  const vector: [BigNumber, BigNumber, BigNumber] = [
-    bignumber(0),
-    bignumber(2),
-    bignumber(3),
-  ];
   let matrix: BigNumber[][] = [
     [bignumber(-2), bignumber(0), bignumber(4)],
     [bignumber(3), bignumber(1), bignumber(-4)],
     [bignumber(0), bignumber(0), bignumber(3)],
   ];
+  const vector: [BigNumber, BigNumber, BigNumber] = [
+    bignumber(0),
+    bignumber(2),
+    bignumber(3),
+  ];
 
+  // Transposing the matrix so we can expect a conventional matrix vector multiplication
   matrix = transpose(matrix);
 
   const result = multiplyVectorMatrix(vector, matrix);
-  expect(result).toEqual([12, -10, 9]);
+  expect(result).toEqual([bignumber(12), bignumber(-10), bignumber(9)]);
 });
 
-test("NEW APPROACH", () => {
+test("Custom matrix * vector multiplication", () => {
+  const matrix: BigNumber[][] = [
+    [bignumber(-2), bignumber(0), bignumber(4)],
+    [bignumber(3), bignumber(1), bignumber(-4)],
+    [bignumber(0), bignumber(0), bignumber(3)],
+  ];
   const vector: [BigNumber, BigNumber, BigNumber] = [
-    bignumber(0.1),
     bignumber(0),
-    bignumber(0),
+    bignumber(2),
+    bignumber(3),
   ];
 
+  const result = multiplyVectorMatrix(vector, matrix);
+  expect(result).toEqual([bignumber(6), bignumber(2), bignumber(1)]);
+});
+
+test("Custom matrix * vector multiplication with decimals", () => {
   const matrix: BigNumber[][] = [
     [bignumber(1), bignumber(0.2), bignumber(0.3)],
     [bignumber(0.1), bignumber(1), bignumber(0.2)],
     [bignumber(0.3), bignumber(-0.1), bignumber(1)],
   ];
 
+  const vector: [BigNumber, BigNumber, BigNumber] = [
+    bignumber(0.1),
+    bignumber(0),
+    bignumber(0),
+  ];
+
   const result = multiplyVectorMatrix(vector, matrix);
-  expect(result).toEqual([0.1, 0.02, 0.03]);
+  expect(result).toEqual([bignumber(0.1), bignumber(0.02), bignumber(0.03)]);
   const result2 = multiplyVectorMatrix(result, matrix);
-  expect(result2).toEqual([0.111, 0.037, 0.064]);
+  expect(result2).toEqual([
+    bignumber(0.111),
+    bignumber(0.037),
+    bignumber(0.064),
+  ]);
   const result3 = multiplyVectorMatrix(result2, matrix);
-  expect(result3).toEqual([0.1339, 0.0528, 0.1047]);
+  expect(result3).toEqual([
+    bignumber(0.1339),
+    bignumber(0.0528),
+    bignumber(0.1047),
+  ]);
 });
