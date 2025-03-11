84d606456db10c7cc5588f9e6e00efe59c304f9c
Hannes Koksch <hannes.koksch@gmail.com>
2025-03-11 19:43:52 +0100

    migrate line chart to react-chartjs-2

--- src/experimental/matrix-vector-multiplication/ProgressionLineChart.tsx
+++ src/experimental/matrix-vector-multiplication/ProgressionLineChart.tsx
@@ -1,7 +1,7 @@
+import "chart.js/auto";
+import { ChartOptions } from "chart.js/auto";
 import { BigNumber } from "mathjs";
-import Chart, { ChartOptions } from "chart.js/auto";
-import { useEffect, useRef } from "react";
-
+import { Line } from "react-chartjs-2";
 interface IProps {
   vectors: BigNumber[][];
   variables: string[] | number[];
@@ -13,56 +13,37 @@ function ProgressionLineChart({
   variables,
   maintainAspectRatio = true,
 }: IProps) {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const labels = vectors.map((_, index) => `Round ${index}`);
 
-  useEffect(() => {
-    const labels = vectors.map((_, index) => `Round ${index}`);
+  const data = {
+    labels: labels,
+    datasets: variables.map((variable, variableIndex) => ({
+      label: variable.toString(),
+      data: vectors.map((vector) => {
+        return vector[variableIndex].toNumber();
+      }),
+    })),
+  };
 
-    if (!canvasRef.current) return;
-
-    const ctx = canvasRef.current.getContext("2d");
-
-    const data = {
-      labels: labels,
-      datasets: variables.map((variable, variableIndex) => ({
-        label: variable.toString(),
-        data: vectors.map((vector) => {
-          return vector[variableIndex].toNumber();
-        }),
-      })),
-    };
-
-    const options: ChartOptions = {
-      responsive: true,
-      maintainAspectRatio: maintainAspectRatio,
-      plugins: {
-        legend: {
-          display: true,
-        },
+  const options: ChartOptions<"line"> = {
+    responsive: true,
+    maintainAspectRatio: maintainAspectRatio,
+    plugins: {
+      legend: {
+        display: true,
       },
-      elements: {
-        point: {
-          radius: 6,
-          hoverRadius: 8,
-        },
+    },
+    elements: {
+      point: {
+        radius: 6,
+        hoverRadius: 8,
       },
-    };
-
-    const myChart = new Chart(ctx!, {
-      type: "line",
-      data: data,
-      options: options,
-    });
-
-    // Cleanup function to destroy chart instance
-    return () => {
-      myChart.destroy();
-    };
-  }, [variables, vectors, maintainAspectRatio]);
+    },
+  };
 
   return (
     <div className="max-w-[800px] max-h-[400px]">
-      <canvas ref={canvasRef}></canvas>
+      <Line data={data} options={options} id={vectors.toString()} />
     </div>
   );
 }
