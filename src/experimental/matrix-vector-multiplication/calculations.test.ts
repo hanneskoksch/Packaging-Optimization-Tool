811d2430707c13e26e2849b41f120a8547768061
Hannes Koksch <hannes.koksch@gmail.com>
2025-03-13 21:31:17 +0100

    test: calculations, 100 % test coverage

--- src/experimental/matrix-vector-multiplication/calculations.test.ts
+++ src/experimental/matrix-vector-multiplication/calculations.test.ts
@@ -1,8 +1,11 @@
 import { expect, test } from "vitest";
-import { multiplyVectorMatrix } from "./calculations";
+import {
+  calculateVectorProgression,
+  multiplyVectorMatrix,
+} from "./calculations";
 import { bignumber, BigNumber, transpose } from "mathjs";
 
-test("Easy matrix * vector multiplication", () => {
+test("Multiply vector matrix - Easy matrix * vector multiplication", () => {
   let matrix: BigNumber[][] = [
     [bignumber(-2), bignumber(0), bignumber(4)],
     [bignumber(3), bignumber(1), bignumber(-4)],
@@ -21,7 +24,7 @@ test("Easy matrix * vector multiplication", () => {
   expect(result).toEqual([bignumber(12), bignumber(-10), bignumber(9)]);
 });
 
-test("Custom matrix * vector multiplication", () => {
+test("Multiply vector matrix - Custom matrix * vector multiplication", () => {
   const matrix: BigNumber[][] = [
     [bignumber(-2), bignumber(0), bignumber(4)],
     [bignumber(3), bignumber(1), bignumber(-4)],
@@ -37,7 +40,7 @@ test("Custom matrix * vector multiplication", () => {
   expect(result).toEqual([bignumber(6), bignumber(2), bignumber(1)]);
 });
 
-test("Custom matrix * vector multiplication with decimals", () => {
+test("Multiply vector matrix - Custom matrix * vector multiplication with decimals", () => {
   const matrix: BigNumber[][] = [
     [bignumber(1), bignumber(0.2), bignumber(0.3)],
     [bignumber(0.1), bignumber(1), bignumber(0.2)],
@@ -63,5 +66,43 @@ test("Custom matrix * vector multiplication with decimals", () => {
     bignumber(0.1339),
     bignumber(0.0528),
     bignumber(0.1047),
+  ]);
+});
+
+test("Multiply vector matrix - Wrong matrix dimensions", () => {
+  const matrix: BigNumber[][] = [
+    [bignumber(1), bignumber(0.2), bignumber(0.3)],
+    [bignumber(0.1), bignumber(1), bignumber(0.2)],
+  ];
+  const vector: [BigNumber, BigNumber, BigNumber] = [
+    bignumber(0.1),
+    bignumber(0),
+    bignumber(0),
+  ];
+
+  expect(() => multiplyVectorMatrix(vector, matrix)).toThrowError(
+    "The matrix dimensions do not match the vector dimensions.",
+  );
+});
+
+test("Vector progression", () => {
+  const matrix: BigNumber[][] = [
+    [bignumber(1), bignumber(0.2), bignumber(0.3)],
+    [bignumber(0.1), bignumber(1), bignumber(0.2)],
+    [bignumber(0.3), bignumber(-0.1), bignumber(1)],
+  ];
+  const vector: [BigNumber, BigNumber, BigNumber] = [
+    bignumber(0.1),
+    bignumber(0),
+    bignumber(0),
+  ];
+
+  const result = calculateVectorProgression(vector, matrix, 4);
+  expect(result).toEqual([
+    [bignumber(0.1), bignumber(0), bignumber(0)],
+    [bignumber(0.1), bignumber(0.02), bignumber(0.03)],
+    [bignumber(0.111), bignumber(0.037), bignumber(0.064)],
+    [bignumber(0.1339), bignumber(0.0528), bignumber(0.1047)],
+    [bignumber(0.17059), bignumber(0.06911), bignumber(0.15543)],
   ]);
 });
